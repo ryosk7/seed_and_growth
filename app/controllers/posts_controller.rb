41014a91818daee0e74e127d438920b5438fd336
Ryosuke Uchida <uchiryo7@gmail.com>
2017-11-16 00:07:05 +0900

    リファクタリングしました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,43 +1,50 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+
   def index
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find_by(id: params[:id])
-  end
+  def show; end
 
   def new
     @post = Post.new
   end
 
   def create
-    @post = Post.new(title: params[:title], content: params[:content])
+    @post = Post.new(post_params)
     if @post.save
-      redirect_to("/posts/#{@post.id}")
+      redirect_to post_path(@post)
     else
-      render("posts/new")
+      render new_post_path
     end
   end
 
-  def edit
-    @post = Post.find_by(id: params[:id])
-  end
+  def edit; end
 
   def update
     @post = Post.find_by(id: params[:id])
     @post.title = params[:title]
     @post.content = params[:content]
     if @post.save
-      redirect_to("/posts/#{@post.id}")
+      redirect_to post_path(@post)
     else
-      render("posts/#{@post.id}/edit")
+      render edit_post_path(@post)
     end
   end
 
   def destroy
-    @post = Post.find_by(id: params[:id])
     @post.destroy
-    redirect_to("/posts/index")
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def post_params
+    params.permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index' => "posts#index"
-  get 'posts/new' => "posts#new"
-  get 'posts/:id' => "posts#show"
-  post 'posts/create' => "posts#create"
-  get 'posts/:id/edit' => "posts#edit"
-  post 'posts/:id/update' => "posts#update"
-  post 'posts/:id/destroy' => "posts#destroy"
+  resources :posts
 
   root 'homes#index'
 end
