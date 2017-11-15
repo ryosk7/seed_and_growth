33ac382b58f4b4fad680b7c5c0cfc2f1474e373c
Ryosuke Uchida <uchiryo7@gmail.com>
2017-11-15 22:22:16 +0900

    編集機能の追加、editページの追加、直前の内容を表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,4 +19,25 @@ class PostsController < ApplicationController
       render("posts/new")
     end
   end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.title = params[:title]
+    @post.content = params[:content]
+    if @post.save
+      redirect_to("/posts/#{@post.id}")
+    else
+      render("posts/#{@post.id}/edit")
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get 'posts/index' => "posts#index"
   get 'posts/new' => "posts#new"
-  post 'posts/create' => "posts#create"
   get 'posts/:id' => "posts#show"
+  post 'posts/create' => "posts#create"
+  get 'posts/:id/edit' => "posts#edit"
+  post 'posts/:id/update' => "posts#update"
+  post 'posts/:id/destroy' => "posts#destroy"
+
 
   get '/' => "homes#top"
 
