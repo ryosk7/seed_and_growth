03e7a8e42fbd1b7ccb875f441218f3849df82d54
Ryosuke Uchida <uchiryo7@gmail.com>
2017-11-20 02:30:32 +0900

    画像投稿におけるバリデーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
+  validates :picture, presence: true
   validates :content, presence: true, length: { maximum: 185 }
   validates :title, presence: true, length: { maximum: 46 }
   validates :article, presence: true
